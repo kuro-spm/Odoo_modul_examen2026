dc9ef0fa11b928d061533f5c99a3dbd2836ac5f4
kuro-spm <sarapratsm@gmail.com>
2026-05-26 16:38:04 +0200

    vistes a mitges

--- __manifest__.py
+++ __manifest__.py
@@ -23,6 +23,8 @@
         'security/ir.model.access.csv', 
         'views/actions.xml', 
         'views/menus.xml', 
+        'insurance_policy_views.xml',
+        'insurance_reason_views.xml',
         'report/report.xml'
     ], 
     'installable': True, 
--- models/models.py
+++ models/models.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*- 
-from odoo import models, fields, api, _ 
+from odoo import models, fields, api, _ , tools
 from odoo.exceptions import ValidationError 
+from dateutil.relativedelta import relativedelta
  
 
 class InsurancePolicy(models.Model):
     _name = 'insurance.policy'
     _description = 'Insurance Policy management'
+    _order = 'id desc'
 
     policyholder_id = fields.Many2one('res.partner', string='Policy holder', domain=[('customer_rank', '>', 0)])
-    product_id = fields.Many2one('product.template', string='Product')
-    provider_id = fields.Many2one('res.partner', string='Provider', domain=[('supplir_rank', '>', 0)])
-    #usuari que el crea
+    product_id = fields.Many2one('product.template', string='Insurance')
+    broker_id = fields.Many2one('res.partner', string='Insurance Broker', domain=[('supplir_rank', '>', 0)])
     created_by = fields.Many2one('res.users', string='Created by', default=lambda self: self.env.user)
-    #data de creació    
     created_date = fields.Datetime(string='Created Date', default=fields.Datetime.now)
-
-    description = fields.Text(' Description')
-
-    #data efectiva
-    effective_date = fields.Date(string='Effective Date')
+    object = fields.Text('Insured Object')
     cancellation_date = fields.Date(string='Cancellation Date')
-
+    effective_date = fields.Date(string='Effective Date')
+    effective_hour = fields.Float(string='Effective Hour')
+    expiration_date = fields.Date(string='Expiration Date')
+    reason_id = fields.Many2one('insurance.reason', string='Reason for cancellation')
     active = fields.Boolean(string='Active', default=True)
 
     type = fields.Selection([
@@ -28,15 +27,35 @@ class InsurancePolicy(models.Model):
         ('mensual', 'Mensual'),
         ('semestral', 'Semestral'),
         ('trimestral', 'Trimestral')  
-    ], string='type')  
+    ], string='Type of expiration', default='anual') 
 
+    def _compute_expiration_date(self):
+        for record in self:
+            if record.effective_date and record.type:
+                if record.type == 'anual':
+                    record.expiration_date = record.effective_date + relativedelta(years=1)
+                elif record.type == 'mensual':
+                    record.expiration_date = record.effective_date + relativedelta(months=1)
+                elif record.type == 'semestral':
+                    record.expiration_date = record.effective_date + relativedelta(months=6)
+                elif record.type == 'trimestral':
+                    record.expiration_date = record.effective_date + relativedelta(months=3)
+            else:
+                record.expiration_date = False 
+
+    # cancellation date cannot be before effective date
     @api.constrains('cancellation_date', 'effective_date')
-    def _check_dates(self):
+    def _check_cancellation_date(self):
         for record in self:
             if record.cancellation_date and record.effective_date and record.cancellation_date < record.effective_date:
                 raise ValidationError(_('Cancellation date cannot be before effective date.'))
-            
-    reason_id = fields.Many2one('insurance.reason', string='Reason for cancellation')
+    
+    # efective date cannot be in the past
+    @api.constrains('effective_date')
+    def _check_effective_date(self):
+        for record in self:
+            if record.effective_date and record.effective_date < fields.Date.today():
+                raise ValidationError(_('Effective date cannot be in the past.'))       
 
     def unlink(self):
         #cannot be deleted
@@ -48,7 +67,7 @@ class InsurancePolicy(models.Model):
     def _check_active(self):
         for record in self:
             if not record.active:
-                if not record.cancellation_date:
+                if not record.expiration_date:
                     raise ValidationError(_('Cancellation date must be set to set policy as inactive.'))
                 if not record.reason_id:
                     raise ValidationError(_('Reason for cancellation must be set to set policy as inactive.'))
@@ -60,7 +79,7 @@ class InsurancePolicy(models.Model):
                 record.display_name = record.id + " - " + record.policyholder_id.name + " - " + record.product_id.name
             else:
                 record.display_name = super(InsurancePolicy, record)._compute_display_name()
-                
+
 
 
     
@@ -68,6 +87,17 @@ class InsurancePolicy(models.Model):
 class InsuranceReason(models.Model):
     _name = 'insurance.reason'
     _description = 'Insurance Reason management'
+    
+    description = fields.Text(string='Reason for cancellation', required=True)
+    #unique index on description
+    created_date = fields.Datetime(string='Created Date', default=fields.Datetime.now)
+
+
+    def _auto_init(self):
+        res = super(InsuranceReason, self)._auto_init()
+        tools.create_unique_index(self._cr, 'insurance_reason_description_uniq', self.ids, ['lower(description)'])
+      
+    
 
 
     
